a49d2c0df54b10f5a8d9a32766136f6a54d69a5a
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2024-06-06 17:00:38 +0200

    Initial state

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "<h1>Hello world!</h1>";
+
+$mysqli = new mysqli(
+   'localhost',
+   'my_user',
+   'my_pass',
+   'my_db' 
+);
+
+if (mysqli_connect_error()) {
+   echo '<h2>Connect Error ' . mysqli_connect_errno() . ': ' . mysqli_connect_error() . '</h2>';
+} else {
+   echo "<h2>Connected to the database!</h2>";
+}
+
+
+error_log(date('Y-m-d-m-Y-H-i-s') . ': An important event happened'.PHP_EOL, 3, __DIR__ . '/logs/important.log');
+
+
+echo "<h2>Event logged</h2>";
+
+
